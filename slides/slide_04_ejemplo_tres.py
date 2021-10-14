3b613a2b9b60d7c8f3dcf38848e6cacd93f0ebc0
sebastiandres <sebastiandres@gmail.com>
2021-10-13 21:05:33 -0300

    getting the images

--- slides/slide_04_ejemplo_tres.py
+++ slides/slide_04_ejemplo_tres.py
@@ -3,63 +3,54 @@ import pandas as pd
 import numpy as np
 import cv2
 
+import os
+import numpy as np
+from tensorflow.keras.models import load_model
+from streamlit_drawable_canvas import st_canvas
+
 from code.shared_functions import skip_echo
+
+#@st.cache 
+def load_digits_model():
+    model = load_model('model')
+    return model
 
 def display(my_title = "", show_code=False):
     with st.echo("below") if show_code else skip_echo():
-        st.title("Ejemplo 3 - ?")
+        c1, c2 = st.columns([10,1])
+        c1.title("Ejemplo 3 - ?")
+        show_code = c2.checkbox("Código")
+        st.caption("Webapp de XYZ - http://www.github.com/XYZ")
+        with st.echo("above") if show_code else skip_echo():
 
-        import os
-        import numpy as np
-        from tensorflow.keras.models import load_model
-        #import streamlit as st
-        from streamlit_drawable_canvas import st_canvas
+            model = load_digits_model()
 
-        #MODEL_DIR = os.path.join(os.path.dirname(__file__), 'model')
-        #if not os.path.isdir(MODEL_DIR):
-        #    os.system('runipy train.ipynb')
+            c1, c2 = st.columns([3,6])
 
-        model = load_model('model')
+            with c1:
+                st.markdown("Dibuja un dígito")
+                SIZE = 28*10
+                canvas_result = st_canvas(
+                    fill_color='#000000',
+                    stroke_width=20,
+                    stroke_color='#FFFFFF',
+                    background_color='#000000',
+                    width=SIZE,
+                    height=SIZE,
+                    drawing_mode="freedraw", #if mode else "transform",
+                    display_toolbar=True,
+                    key='canvas')
 
-        st.title('My Digit Recognizer')
-        st.markdown('''
-        Try to write a digit!
-        ''')
-
-        # data = np.random.rand(28,28)
-        # img = cv2.resize(data, (256, 256), interpolation=cv2.INTER_NEAREST)
-
-        c1, c2, c3 = st.columns([3,3,3])
-
-        with c1:
-            SIZE = 28*10
-            #mode = st.checkbox("Draw (or Delete)?", True)
-            canvas_result = st_canvas(
-                fill_color='#000000',
-                stroke_width=20,
-                stroke_color='#FFFFFF',
-                background_color='#000000',
-                width=SIZE,
-                height=SIZE,
-                drawing_mode="freedraw", #if mode else "transform",
-                display_toolbar=True,
-                key='canvas')
-
-        if c2.button("Resize"):
-            img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
-            rescaled = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
-            c2.write('Model Input')
-            c2.image(rescaled)
-
-        if c3.button("Predict"):       
-            img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
-            rescaled = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
-            c2.write('Model Input')
-            c2.image(rescaled)
-            test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            val = model.predict(test_x.reshape(1, 28, 28))
-            c3.write(f'result: {np.argmax(val[0])}')
-            c3.bar_chart(val[0])
+            if c1.button("Reconocer dígito"):       
+                img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
+                #rescaled = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
+                #c2.write('Model Input')
+                #c2.image(rescaled)
+                c2.image(img)
+                test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                val = model.predict(test_x.reshape(1, 28, 28))
+                c2.write(f'Predicción: {np.argmax(val[0])}')
+                c2.bar_chart(val[0])
 
 
 if __name__=="__main__":
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import importlib
 import glob
 
 # Parameters
-STARTING_SLIDE = 8
+STARTING_SLIDE = 0
 
 # Config and setup
 st.set_page_config(layout="wide")
@@ -22,11 +22,11 @@ if c3.button(">") and st.session_state.slide_number<N-1:
 c2.write(f"{st.session_state.slide_number}") # Put it after the buttons, so it gets updated accordingly
 if st.session_state.slide_number>=2:
     L.markdown("###### **WebApps con Streamlit : ¡más fácil que la tabla del uno, poh!**")
-    L.markdown("###### Pycon Chile, Noviembre 2021, por Sebastián Flores [^T^L]/[@sebastiandres](https://github.com/sebastiandres) (GH, TW, LI) | [sebastiandres.xyz](sebastiandres.xyz)")
-# https://img.shields.io/badge/-FFFFFF?logo=twitter
-# https://img.shields.io/badge/-FFFFFF?logo=github
-# https://img.shields.io/badge/-FFFFFF?logo=linkedin
-
+    L.markdown("Pycon Chile, Noviembre 2021, por Sebastián Flores ![this is an image link](./images/streamlit.png) @sebastiandres [sebastiandres.xyz](sebastiandres.xyz)")
+    L.image("github.svg")
+    L.markdown(
+        "Made in [![this is an image link](https://i.imgur.com/iIOA6kU.png)](https://www.streamlit.io/)&nbsp, with :heart: by [@DataChaz](https://www.charlywargnier.com/) &nbsp | &nbsp [![Follow](https://img.shields.io/twitter/follow/datachaz?style=social)](https://www.twitter.com/datachaz) &nbsp | &nbsp [![this is an image link](https://i.imgur.com/thJhzOO.png)](https://www.buymeacoffee.com/cwar05)"
+    )
 
 # Display the slides
 module_str = slide_files[st.session_state.slide_number].replace("/",".").replace(".py","")
